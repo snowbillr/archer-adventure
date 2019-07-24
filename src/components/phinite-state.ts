9639b765a1f128e8ab64cfc6615e9de5c6b4c1d1
Bill Reed <billnreed@gmail.com>
2019-07-24 00:30:34 -0400

    tweak

--- src/components/phinite-state.ts
+++ src/components/phinite-state.ts
@@ -16,13 +16,11 @@ export class PhiniteState<T extends HasPhiniteState<T>> implements PhiniteState.
   }
 
   create() {
-    const transitionEventTypes = [Phaser.Input.Keyboard.Events.ANY_KEY_DOWN, Phaser.Input.Keyboard.Events.ANY_KEY_UP];
-
-    transitionEventTypes.forEach(eventType => {
+    const transitionKeyboardEventTypes = [Phaser.Input.Keyboard.Events.ANY_KEY_DOWN, Phaser.Input.Keyboard.Events.ANY_KEY_UP];
+    transitionKeyboardEventTypes.forEach(eventType => {
       this.scene.input.keyboard.on(eventType, (e: any) => {
         const transition = this.currentState.transitions
-          .filter(transition => transition.event === eventType)
-          .find(transition => transition.key === e.key);
+          .find(transition => transition.event === eventType && transition.key === e.key);
 
         if (transition) {
           this.currentState = <PhiniteState.State<T>> this.states.find(state => state.id === transition.to);
