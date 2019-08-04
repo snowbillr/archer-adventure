569acd45df357c55d52c00979b3caec25d277946
Bill Reed <billnreed@gmail.com>
2019-08-04 13:00:01 -0400

    multiple rectangle hitboxes per frame

--- src/components/hitboxes.ts
+++ src/components/hitboxes.ts
@@ -4,9 +4,10 @@ export class Hitboxes<T extends (PhysicallyRenderable.Entity | Renderable.Entity
   private animationsKey: string;
   private hitboxFrames!: [];
 
-  private rectangle!: Phaser.Geom.Rectangle;
+  private rectanglePool: Phaser.Geom.Rectangle[];
+  private activeRectangles: Phaser.Geom.Rectangle[];
 
-  private debugRectangle!: Phaser.GameObjects.Rectangle;
+  private debugRectangles: Phaser.GameObjects.Rectangle[];
   private debug: boolean;
   private debugPointerPosition: any;
   private debugColor: number;
@@ -15,15 +16,19 @@ export class Hitboxes<T extends (PhysicallyRenderable.Entity | Renderable.Entity
     this.scene = scene;
     this.entity = entity;
     this.animationsKey = animationsKey;
+
+    this.rectanglePool = [];
+    this.activeRectangles = [];
+
     this.debug = debug;
     this.debugColor = 0x00FF00;
+    this.debugRectangles = [];
   }
 
   create() {
     this.hitboxFrames = this.scene.cache.json.get(this.animationsKey).frames;
-    this.rectangle = new Phaser.Geom.Rectangle(0, 0, 0, 0);
 
-    this.debugRectangle = this.scene.add.rectangle(0, 0, 0, 0, this.debugColor, 0.5);
+    this.debugRectangles = []
     this.debugPointerPosition = { x: 0, y: 0 };
     this.scene.input.on('pointermove', (e: any) => {
       this.debugPointerPosition = e.position;
@@ -31,46 +36,84 @@ export class Hitboxes<T extends (PhysicallyRenderable.Entity | Renderable.Entity
   }
 
   update() {
+    this.disableHitboxes();
+
     const key = this.entity.sprite.frame.texture.key;
     const frame = this.entity.sprite.frame.name;
 
     const hitboxDefinition: any = this.hitboxFrames.find((a: Phaser.Types.Animations.JSONAnimationFrame) => a.key === key && a.frame === frame);
     if (hitboxDefinition && hitboxDefinition.hitboxes) {
-      const h = hitboxDefinition.hitboxes[0];
-
-      const scaleX = this.entity.sprite.scaleX;
-      const scaleY = this.entity.sprite.scaleY;
-
-      const width = scaleX * h.width;
-      const height = scaleY * h.height;
-      const x = (this.entity.sprite.x + (h.x * scaleX)) - (width * this.entity.sprite.originX);
-      const y = (this.entity.sprite.y + (h.y * scaleY)) - (height * this.entity.sprite.originY);
-
-      this.rectangle.x = x;
-      this.rectangle.y = y;
-      this.rectangle.width = width;
-      this.rectangle.height = height;
+      hitboxDefinition.hitboxes.forEach((hitbox: any) => {
+        if (hitbox.type === 'rectangle') {
+          this.setRectangleHitbox(hitbox);
+        } else {
+          throw 'unsupported hitbox type';
+        }
+      })
     }
 
-    if (this.debug && hitboxDefinition && hitboxDefinition.hitboxes) {
-      const point = new Phaser.Geom.Point(this.debugPointerPosition.x, this.debugPointerPosition.y);
-      if (Phaser.Geom.Rectangle.ContainsPoint(this.rectangle, point)) {
+    if (this.debug) {
+      this.renderDebugHitboxes();
+    }
+  }
+
+  renderDebugHitboxes() {
+    const point = new Phaser.Geom.Point(this.debugPointerPosition.x, this.debugPointerPosition.y);
+
+    this.activeRectangles.forEach((activeRectangle, i) => {
+      if (Phaser.Geom.Rectangle.ContainsPoint(activeRectangle, point)) {
         this.debugColor = 0xFF0000;
       } else {
         this.debugColor = 0x00FF00;
       }
 
-      this.debugRectangle.visible = true;
+      const debugRectangle = this.debugRectangles[i];
 
-      this.debugRectangle.fillColor = this.debugColor;
+      debugRectangle.visible = true;
+      debugRectangle.fillColor = this.debugColor;
 
-      this.debugRectangle.setOrigin(0, 0);
-      this.debugRectangle.x = this.rectangle.x;
-      this.debugRectangle.y = this.rectangle.y;
-      this.debugRectangle.width = this.rectangle.width;
-      this.debugRectangle.height = this.rectangle.height;
-    } else {
-      this.debugRectangle.visible = false;
+      debugRectangle.setOrigin(0, 0);
+      debugRectangle.x = activeRectangle.x;
+      debugRectangle.y = activeRectangle.y;
+      debugRectangle.width = activeRectangle.width;
+      debugRectangle.height = activeRectangle.height;
+    })
+  }
+
+  disableHitboxes() {
+    this.rectanglePool = [...this.rectanglePool, ...this.activeRectangles];
+    this.activeRectangles = [];
+
+    this.debugRectangles.forEach(r => r.visible = false);
+  }
+
+  getAvailableRectangle() {
+    let rectangle = this.rectanglePool.pop();
+    if (rectangle == null) {
+      rectangle = new Phaser.Geom.Rectangle(0, 0, 0, 0);
+      if (this.debug) {
+        this.debugRectangles.push(this.scene.add.rectangle(0, 0, 0, 0, this.debugColor, 0.5));
+      }
     }
+
+    this.activeRectangles.push(rectangle);
+    return rectangle;
+  }
+
+  setRectangleHitbox(hitbox: any) {
+    const rectangle = this.getAvailableRectangle();
+
+    const scaleX = this.entity.sprite.scaleX;
+    const scaleY = this.entity.sprite.scaleY;
+
+    const width = scaleX * hitbox.width;
+    const height = scaleY * hitbox.height;
+    const x = (this.entity.sprite.x + (hitbox.x * scaleX)) - (width * this.entity.sprite.originX);
+    const y = (this.entity.sprite.y + (hitbox.y * scaleY)) - (height * this.entity.sprite.originY);
+
+    rectangle.x = x;
+    rectangle.y = y;
+    rectangle.width = width;
+    rectangle.height = height;
   }
 }
