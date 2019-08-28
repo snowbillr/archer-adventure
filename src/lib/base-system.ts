a407a7a6b83f39855a004da2ddeee626ada1b550
Bill Reed <billnreed@gmail.com>
2019-08-28 00:15:29 -0400

    base system

---
+++ src/lib/base-system.ts
@@ -0,0 +1,20 @@
+export abstract class BaseSystem<T, U> implements Tags.TagSystem {
+  protected tag1: string;
+  protected tag2: string;
+
+  protected tag1s: T[];
+  protected tag2s: U[];
+
+  constructor(tag1: string, tag2: string) {
+    this.tag1 = tag1;
+    this.tag2 = tag2;
+
+    this.tag1s = [];
+    this.tag2s = [];
+  }
+
+  update(tagManager: Tags.TagManager): void {
+    this.tag1s = tagManager.getEntities(this.tag1) as T[];
+    this.tag2s = tagManager.getEntities(this.tag2) as U[];
+  }
+}
--- src/lib/sign-system.ts
+++ src/lib/sign-system.ts
@@ -1,23 +1,10 @@
+import { BaseSystem } from './base-system';
+
 import { Sign } from '../entities/sign';
 
-export class SignSystem<T extends Interactable.Entity, U extends Sign> {
-  private tag1: string;
-  private tag2: string;
-
-  private tag1s: T[];
-  private tag2s: U[];
-
-  constructor(tag1: string, tag2: string) {
-    this.tag1 = tag1;
-    this.tag2 = tag2;
-
-    this.tag1s = [];
-    this.tag2s = [];
-  }
-
+export class SignSystem<T extends Interactable.Entity> extends BaseSystem<T, Sign> implements Tags.TagSystem {
   update(tagManager: Tags.TagManager) {
-    this.tag1s = tagManager.getEntities(this.tag1) as T[];
-    this.tag2s = tagManager.getEntities(this.tag2) as U[];
+    super.update(tagManager);
 
     const interactors = this.tag1s;
     const signs = this.tag2s;
