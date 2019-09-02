73ea58d8e0e174328003989d978f9dbe0c87cabb
Bill Reed <billnreed@gmail.com>
2019-09-01 20:03:30 -0400

    layer depth

--- src/lib/area-manager/area-manager.ts
+++ src/lib/area-manager/area-manager.ts
@@ -22,8 +22,8 @@ export class AreaManager {
     this.scene = scene;
     this.scale = scale;
 
-    const map = scene.make.tilemap({ key: 'starting-area' });
-    const tileset = map.addTilesetImage('fantasy-platformer-core', 'fantasy-platformer-core');
+    const map = scene.make.tilemap({ key: mapKey });
+    const tileset = map.addTilesetImage(tilesetName, tilesetKey);
 
     this.map = map;
     this.tileset = tileset;
@@ -32,13 +32,18 @@ export class AreaManager {
 
   createTileLayers(layerNames: string[]) {
     layerNames.forEach(layerName => {
+
       const layer = this.map.createStaticLayer(layerName, this.tileset, 0, 0);
       layer.setScale(this.scale);
 
-      const collisionProperty = (layer.layer.properties as any).collisionProperty;
+      const properties = layer.layer.properties as any;
+
+      const collisionProperty = properties.collisionProperty;
       if (collisionProperty) {
         layer.setCollisionByProperty({ [collisionProperty]: true })
       }
+
+      layer.setDepth(properties.depth);
 
       this.layers.push(layer);
     });
@@ -46,7 +51,9 @@ export class AreaManager {
 
   createObjects(layerName: string, systemsManager: SystemsManager.SystemsManager): any[] {
     const createdEntities: any[] = [];
-    const tiledObjects = this.map.getObjectLayer(layerName).objects;
+    const layer = this.map.getObjectLayer(layerName);
+    const layerProperties = layer.properties as any;
+    const tiledObjects = layer.objects;
 
     tiledObjects.forEach((tiledObject: Phaser.Types.Tilemaps.TiledObject) => {
       const entity = {} as any;
