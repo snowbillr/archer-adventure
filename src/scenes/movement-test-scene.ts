30f046384781c669d3b93d8ec47eec282b72f269
Bill Reed <billnreed@gmail.com>
2019-09-03 07:56:55 -0400

    cleanup

--- src/scenes/movement-test-scene.ts
+++ src/scenes/movement-test-scene.ts
@@ -50,16 +50,6 @@ export class MovementTestScene extends BaseScene {
 
     const { tilemapKey, tilesetName, tilesetKey, scale = 2 } = data;
     this.loadNewArea(tilemapKey, tilesetName, tilesetKey, scale);
-    /*
-    this.areaManager.load(tilemapKey, tilesetName, tilesetKey, scale);
-
-    const adventurer = this.areaManager.objects['adventurer'][0];
-    const map = this.areaManager.map;
-    const tileset = this.areaManager.tileset;
-    this.cameras.main.setBackgroundColor(0xCCCCCC);
-    this.cameras.main.setBounds(0, 0, map.width * tileset.tileWidth * 2, map.height * tileset.tileHeight * 2);
-    this.cameras.main.startFollow(adventurer.sprite, true);
-    */
   }
 
   update() {
