301d215b7d038ec3960157fc0c5fdc5e8d670332
Bill Reed <billnreed@gmail.com>
2019-09-04 21:37:45 -0400

    load adventurer separately from the layers

--- src/plugins/area-manager-plugin.ts
+++ src/plugins/area-manager-plugin.ts
@@ -13,6 +13,8 @@ export class AreaManagerPlugin extends Phaser.Plugins.ScenePlugin {
 
   private areaMap: { [areaName: string]: any };
 
+  public adventurer!: Entities.Adventurer;
+
   constructor(scene: Phaser.Scene, pluginManager: Phaser.Plugins.PluginManager) {
     super(scene, pluginManager);
 
@@ -50,6 +52,7 @@ export class AreaManagerPlugin extends Phaser.Plugins.ScenePlugin {
     this.loadMarkers();
     this.createTileLayers(this.map.layers.map(layer => layer.name));
     this.createObjectLayers(this.map.objects.map(layer => layer.name));
+    this.createAdventurer();
   }
 
   unload() {
@@ -115,31 +118,70 @@ export class AreaManagerPlugin extends Phaser.Plugins.ScenePlugin {
     this.objects[layerName] = [];
 
     tiledObjects.forEach((tiledObject: Phaser.Types.Tilemaps.TiledObject) => {
-      const entity = {} as any;
-
-      const tileProperties: any = this.normalizeProperties(tiledObject.properties);
-      tiledObject.properties = tileProperties;
-
-      tileProperties.tags.split(',').forEach((tag: string) => {
-        this.registerEntity(tag, entity, tiledObject);
-      });
-
-      if (tileProperties.layerCollisions) {
-        tileProperties.layerCollisions.split(',').forEach((layerName: string) => {
-          this.scene.physics.add.collider(entity.sprite, this.tileLayers.find(layer => layer.layer.name === layerName)!);
-        });
-      }
-
-      if (layerProperties.depth && entity.sprite) {
-        entity.sprite.setDepth(layerProperties.depth);
-      }
+      const entity = this.createObject(tiledObject.properties, layerProperties.depth, tiledObject.x, tiledObject.y);
 
       this.objects[layerName].push(entity);
     });
   }
 
-  private createPrefabs() {
+  private createObject(properties: { [key: string]: any }, depth?: number, x?: number, y?: number) {
+    const entity = {} as any;
+    const props = this.normalizeProperties(properties);
+    const fakeTiledObject = {
+      x: x,
+      y: y,
+      properties: props,
+    };
 
+    if (props.tags) {
+      props.tags.split(',').forEach((tag: string) => {
+        this.registerEntity(tag, entity, fakeTiledObject as Phaser.Types.Tilemaps.TiledObject);
+      });
+    }
+
+    if (props.layerCollisions) {
+      props.layerCollisions.split(',').forEach((layerName: string) => {
+        this.scene.physics.add.collider(entity.sprite, this.tileLayers.find(layer => layer.layer.name === layerName)!);
+      });
+    }
+
+    if (depth && entity.sprite) {
+      entity.sprite.setDepth(depth);
+    }
+
+    return entity;
+  }
+
+  private createAdventurer() {
+    const adventurerProps = {
+      boundsKey: "adventurer-bounds",
+      frame: 0,
+      hurtboxesDebug: false,
+      hurtboxesKey: "adventurer-hurtboxes",
+      initialStateId: "adventurer-stand",
+      interactionRadius: 30,
+      layerCollisions: "ground",
+      maxVelocityX: 350,
+      stateSet: "adventurer",
+      tags: "hasPhysicalSprite,hasHurtboxes,hasBounds,hasControls,hasInteractionCircle,sign-interactor,hasPhiniteStateMachine,doorInteractor",
+      texture: "adventurer-core",
+    };
+
+   this.adventurer = this.createObject(adventurerProps, 2, 0, 0) as Entities.Adventurer;
+
+    this.placeAdventurerAtStart();
+  }
+
+  private placeAdventurerAtStart() {
+    const mapProperties = this.normalizeProperties(this.map.properties);
+    if (mapProperties.startingMarker) {
+      console.log(mapProperties.startingMarker)
+      console.log(this.markers)
+      const marker = this.markers.find(m => m.name === mapProperties.startingMarker);
+      // const adventurer = this.objects['adventurer'][0];
+      const adventurer = this.adventurer;
+      adventurer.sprite.setPosition(marker!.x, marker!.y - adventurer.sprite.displayHeight / this.scale);
+    }
   }
 
   private registerEntity(tag: string, entity: SystemsManager.Entity, tiledObject: Phaser.Types.Tilemaps.TiledObject) {
@@ -153,7 +195,7 @@ export class AreaManagerPlugin extends Phaser.Plugins.ScenePlugin {
     });
   }
 
-  private normalizeProperties(properties: any) {
+  private normalizeProperties(properties: any): { [key: string]: any } {
     if (Array.isArray(properties)) {
       return properties.reduce((acc: any, propertyMap: any) => {
         acc[propertyMap.name] = propertyMap.value;
