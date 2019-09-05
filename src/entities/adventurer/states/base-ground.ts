214d39af655b1cb37d5b980b418e1c809fa2d82c
Bill Reed <billnreed@gmail.com>
2019-09-04 20:51:38 -0400

    explain hack

--- src/entities/adventurer/states/base-ground.ts
+++ src/entities/adventurer/states/base-ground.ts
@@ -5,6 +5,10 @@ export const baseGround: Partial<PhiniteStateMachine.States.State<Entities.Adven
     {
       type: TransitionType.Conditional,
       condition: (entity: Entities.Adventurer) => {
+        // Sometimes when the adventurer is on the ground touching the tile they will start to 'fall'
+        // no idea why, but their speed upon falling is 18.333. So this threshold doesn't trigger the fall
+        // state unless they are moving faster than that value.
+        // This doesn't negatively impact running off of a platform.
         const threshold = 20;
         if (entity.body.velocity.y > 5) {
           console.log('falling old threshold')
