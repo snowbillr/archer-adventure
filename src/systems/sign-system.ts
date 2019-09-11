f28420dca9a19d4cffbbd2cbbdd11a53da93601a
Bill Reed <billnreed@gmail.com>
2019-09-11 07:46:07 -0400

    better font size

--- src/systems/sign-system.ts
+++ src/systems/sign-system.ts
@@ -28,8 +28,8 @@ export class SignSystem implements SystemsManager.System {
         .setDepth(10)
         .setScrollFactor(0);
 
-      const textboxText = this.scene.add.bitmapText(0, 0, 'compass-18', 'Here is a message');
-      textboxText.setOrigin(0.5);
+      const textboxText = this.scene.add.bitmapText(0, 0, 'compass-24', 'Here is a message');
+      textboxText.setOrigin(0.5).setScale(0.5);
       sign.textboxSprite.add(textboxText);
 
       sign.isTextboxShowing = false;
