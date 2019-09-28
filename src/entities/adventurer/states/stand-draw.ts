71a1e51165ab346366f5f1b17585c4c05c9895e2
Bill Reed <billnreed@gmail.com>
2019-09-28 14:55:08 -0400

    change direction while firing

--- src/entities/adventurer/states/stand-draw.ts
+++ src/entities/adventurer/states/stand-draw.ts
@@ -5,7 +5,7 @@ import { TransitionType } from '../../../lib/phinite-state-machine/transition-ty
 export const adventurerStandDraw: PhiniteStateMachine.States.State<Entities.Adventurer> = StateMerge(baseIdle, {
   id: 'adventurer-stand-draw',
   onEnter(entity: Entities.Adventurer) {
-    entity.sprite.anims.play('adventurer-stand-draw');
+    entity.sprite.anims.play('adventurer-stand-draw', true);
   },
   onUpdate(entity) {
     entity.shotPower += entity.shotChargeRate;
@@ -23,6 +23,24 @@ export const adventurerStandDraw: PhiniteStateMachine.States.State<Entities.Adve
       event: Phaser.Input.Keyboard.Events.ANY_KEY_UP,
       key: entity => entity.codes.attack,
       to: 'adventurer-stand-shoot',
+    },
+    {
+      type: TransitionType.Input,
+      event: Phaser.Input.Keyboard.Events.ANY_KEY_DOWN,
+      key: entity => entity.codes.left,
+      to: 'adventurer-stand-draw',
+      onTransition(entity) {
+        entity.sprite.flipX = true;
+      }
+    },
+    {
+      type: TransitionType.Input,
+      event: Phaser.Input.Keyboard.Events.ANY_KEY_DOWN,
+      key: entity => entity.codes.right,
+      to: 'adventurer-stand-draw',
+      onTransition(entity) {
+        entity.sprite.flipX = false;
+      }
     }
   ]
 });
--- src/entities/adventurer/states/stand-hold.ts
+++ src/entities/adventurer/states/stand-hold.ts
@@ -5,7 +5,7 @@ import { TransitionType } from '../../../lib/phinite-state-machine/transition-ty
 export const adventurerStandHold: PhiniteStateMachine.States.State<Entities.Adventurer> = StateMerge(baseIdle, {
   id: 'adventurer-stand-hold',
   onEnter(entity: Entities.Adventurer) {
-    entity.sprite.anims.play('adventurer-stand-hold');
+    entity.sprite.anims.play('adventurer-stand-hold', true);
   },
   onUpdate(entity) {
     entity.shotPower += entity.shotChargeRate;
@@ -16,6 +16,24 @@ export const adventurerStandHold: PhiniteStateMachine.States.State<Entities.Adve
       event: Phaser.Input.Keyboard.Events.ANY_KEY_UP,
       key: entity => entity.codes.attack,
       to: 'adventurer-stand-shoot',
+    },
+    {
+      type: TransitionType.Input,
+      event: Phaser.Input.Keyboard.Events.ANY_KEY_DOWN,
+      key: entity => entity.codes.left,
+      to: 'adventurer-stand-hold',
+      onTransition: (entity) => {
+        entity.sprite.flipX = true;
+      }
+    },
+    {
+      type: TransitionType.Input,
+      event: Phaser.Input.Keyboard.Events.ANY_KEY_DOWN,
+      key: entity => entity.codes.right,
+      to: 'adventurer-stand-hold',
+      onTransition: (entity) => {
+        entity.sprite.flipX = false;
+      }
     }
   ]
 });
