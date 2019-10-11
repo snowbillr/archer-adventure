b113976f45f74054933ece767037055283508b2b
Bill Reed <billnreed@gmail.com>
2019-10-11 09:13:37 -0600

    door system uses interaciton tracker

--- src/systems/door-system.ts
+++ src/systems/door-system.ts
@@ -41,14 +41,14 @@ export class DoorSystem implements SystemsManager.System {
     const doors: Systems.DoorSystem.DoorEntity[] = systemsManager.getEntities(DoorSystem.SystemTags.door);
 
     for (let doorInteractor of doorInteractors) {
-      const enteringDoorIds = doorInteractor.enteringInteractionIds;
-      const enteringDoors = doors.filter(door => enteringDoorIds.has(door.id));
+      const enteringDoorIds = doorInteractor.interactionTracker.getEntityIds('entering');
+      const enteringDoors = doors.filter(door => enteringDoorIds.includes(door.id));
       for (let enteringDoor of enteringDoors) {
         enteringDoor.showIndicator();
       }
 
-      const exitingDoorIds = doorInteractor.exitingInteractionIds;
-      const exitingDoors = doors.filter(door => exitingDoorIds.has(door.id));
+      const exitingDoorIds = doorInteractor.interactionTracker.getEntityIds('exiting');
+      const exitingDoors = doors.filter(door => exitingDoorIds.includes(door.id));
       for (let enteringDoor of exitingDoors) {
         enteringDoor.hideIndicator();
       }
