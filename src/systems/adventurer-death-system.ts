bce1a3820b4d567bb214c37a7883cdfd2b015c10
Bill Reed <billnreed@gmail.com>
2019-12-12 07:43:11 -0500

    some cleanup

--- src/systems/adventurer-death-system.ts
+++ src/systems/adventurer-death-system.ts
@@ -1,7 +1,4 @@
-import 'phaser';
-import { AdventurerComponent } from '../components/adventurer-component';
 import { EntityManager } from '../lib/phecs/entity-manager';
-import { HealthComponent } from '../components/health-component';
 import { BaseScene } from '../scenes/base-scene';
 
 export class AdventurerDeathSystem implements Phecs.System {
@@ -12,8 +9,6 @@ export class AdventurerDeathSystem implements Phecs.System {
   }
 
   start(phEntities: EntityManager) {
-    const adventurer = phEntities.getEntitiesByComponent(AdventurerComponent)[0];
-
     this.scene.persistence.onChange<number>('adventurer.health', health => {
       if (health <= 0) {
         this.scene.scene.stop('hud');
