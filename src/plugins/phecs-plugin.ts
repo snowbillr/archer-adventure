c6bdb881b01472f0f59c4676c0c1013fa9984ae0
Bill Reed <billnreed@gmail.com>
2019-12-19 07:41:59 -0500

    remove unused component manager

--- src/plugins/phecs-plugin.ts
+++ src/plugins/phecs-plugin.ts
@@ -1,17 +1,14 @@
 import { EntityManager } from '../lib/phecs/entity-manager';
-import { ComponentManager } from '../lib/phecs/component-manager';
 import { SystemsManager } from '../lib/phecs/systems-manager';
 
 export class PhecsPlugin extends Phaser.Plugins.ScenePlugin {
   public phEntities: EntityManager;
-  public phComponents: ComponentManager;
   public phSystems: SystemsManager;
 
   constructor(scene: Phaser.Scene, pluginManager: Phaser.Plugins.PluginManager) {
     super(scene, pluginManager);
 
     this.phEntities = new EntityManager(scene);
-    this.phComponents = new ComponentManager();
     this.phSystems = new SystemsManager(scene);
   }
 
