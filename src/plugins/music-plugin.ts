ca4b934931e919879fb07a2d5be4a556b753625f
Bill Reed <billnreed@gmail.com>
2020-01-27 23:34:30 -0500

    cache music by key

--- src/plugins/music-plugin.ts
+++ src/plugins/music-plugin.ts
@@ -2,31 +2,32 @@ import { SCENE_KEYS } from "../constants/scene-keys";
 import { MusicRegistrar } from "../registrars/music-registrar";
 
 export class MusicPlugin extends Phaser.Plugins.ScenePlugin {
-  private currentArea: string;
-  private areaMusic: Record<string, Phaser.Sound.BaseSound>;
+  private currentMusicKey: string;
+  private keyedMusic: Record<string, Phaser.Sound.BaseSound>;
 
   constructor(scene: Phaser.Scene, pluginManager: Phaser.Plugins.PluginManager) {
     super(scene, pluginManager);
 
-    this.currentArea = '';
-    this.areaMusic = {};
+    this.currentMusicKey = '';
+    this.keyedMusic = {};
   }
 
   playForArea(areaKey: string) {
-    if (MusicRegistrar.getMusicForArea(this.currentArea) === MusicRegistrar.getMusicForArea(areaKey)) {
+    const newAreaMusicKey = MusicRegistrar.getMusicForArea(areaKey);
+    if (this.currentMusicKey === newAreaMusicKey) {
       return;
     }
 
-    if (this.currentArea) {
-      this.areaMusic[this.currentArea].stop();
+    if (this.currentMusicKey) {
+      this.keyedMusic[this.currentMusicKey].stop();
     }
 
-    this.currentArea = areaKey;
-
-    if (!this.areaMusic[this.currentArea]) {
-      this.areaMusic[this.currentArea] = this.scene.sound.add(MusicRegistrar.getMusicForArea(this.currentArea), { loop: true });
+    if (!this.keyedMusic[newAreaMusicKey]) {
+      this.keyedMusic[newAreaMusicKey] = this.scene.sound.add(newAreaMusicKey, { loop: true });
     }
 
-    this.areaMusic[this.currentArea].play();
+    this.keyedMusic[newAreaMusicKey].play();
+
+    this.currentMusicKey = newAreaMusicKey;
   }
 }
